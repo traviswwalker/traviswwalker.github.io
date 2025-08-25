a76d97b33883131f636d605f58202778a137dc0a
traviswwalker <105796933+traviswwalker@users.noreply.github.com>
2025-08-24 20:26:26 -0600

    Add files via upload

---
+++ inc/junk/meta.php
@@ -0,0 +1,24 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <html lang="en" class=" ">
+  <meta charset='utf-8' />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="shortcut icon" href="http://engineering.oregonstate.edu/sites/all/themes/coe/favicon.ico" type="image/x-icon" />
+  <!-- css -->
+  <link type="text/css" rel="stylesheet" media="all" href="http://oregonstate.edu/tools/css/yui/2.7.0/reset-fonts-grids.css" />
+  <link type="text/css" rel="stylesheet" media="all" href="http://oregonstate.edu/tools/css/osu-base/0.2-beta/osu-base.css" />
+  <link type="text/css" rel="stylesheet" media="all" href="http://oregonstate.edu/osuhomepage/css/css.php?osu-base,top-hat" />
+  <link type="text/css" rel="stylesheet" media="all" href="/inc/styles.css" />
+  <link type="text/css" rel="stylesheet" media="print" href="/inc/print.css" />
+  <link type="text/css" rel="stylesheet" media="all" href="/inc/nivo-slider/nivo-slider.css" />
+  <link rel="stylesheet" type="text/css" href="/inc/jQuery.popeye/css/popeye/jquery.popeye.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="/inc/jQuery.popeye/css/popeye/jquery.popeye.style.css" media="screen" />
+
+<!--[if IE]>
+<link type="text/css" rel="stylesheet" media="all" href="http://engineering.oregonstate.edu/sites/engineering.oregonstate.edu/themes/osu_base/ie.css?5" />
+<![endif]-->
+  
+<!--  javascripts -->
+<script src="http://code.jquery.com/jquery-latest.js"></script> 
+<script type="text/javascript" src="http://oregonstate.edu/osuhomepage/js/js.php?timers,top-hat"></script>
+<script type="text/javascript" src="/inc/jQuery.popeye/lib/popeye/jquery.popeye-2.1.js"></script>
+<script type="text/javascript" src="/inc/nivo-slider/jquery.nivo.slider.pack.js" ></script>
